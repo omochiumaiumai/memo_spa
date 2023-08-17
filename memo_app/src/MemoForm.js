38db7910e7fc1c98225065a86e3d4a5ccbe2f111
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-08-18 01:36:42 +0900

    Button.jsを作成

--- memo_app/src/MemoForm.js
+++ memo_app/src/MemoForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from './Button.js';
+
 export default function MemoForm() {
   return (
     <form>
       <input type="text" name="memo" placeholder="Please enter a note." />
-      <button type="button">Add</button>
+      <Button />
     </form>
   );
 }
