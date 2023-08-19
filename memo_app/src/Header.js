fb7db45c630b1ab939c5b9cccb2473363fb3235a
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-08-20 01:03:41 +0900

    exportの修正

--- memo_app/src/Header.js
+++ memo_app/src/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({ children }) => {
+export default function Header({ children }) {
   return <h1>{children}</h1>;
-};
+}
 
 Header.propTypes = {
   children: PropTypes.string,
 };
-export default Header;
--- memo_app/src/Memo.js
+++ memo_app/src/Memo.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Memo = ({ content, onClick }) => {
+export default function Memo({ content, onClick }) {
   const firstLine = content.split('\n')[0];
   return (
     <div onClick={onClick}>
       <li>{firstLine}</li>
     </div>
   );
-};
+}
 
 Memo.propTypes = {
   content: PropTypes.string,
   onClick: PropTypes.func,
 };
-export default Memo;
